897a9dce42b29253c0ca995f82a6a1b44c55f281
Mary Waithera <njihiamary11@gmail.com>
2020-07-08 21:44:06 +0300

    Use class instead function view to get single posts by primary key

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.posts_home, name='posts_home'),
     path('create/', views.create_post, name='create'),
     path('update/', views.update_post, name='update'),
     path('delete/', views.delete_post, name='delete'),
-    path('post/<int:pk>', views.get_single_post, name='single_post'),
+    path('<int:pk>/', views.DetailView.as_view(), name='details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from blog.models import Post
 from django.http import HttpResponse, Http404
 from django.core.exceptions import ObjectDoesNotExist
+from django.views import generic
 
 def posts_home(request):
     posts = Post.objects.all()
@@ -10,14 +11,10 @@ def posts_home(request):
     }
     return render(request, 'blog/home.html', context)
 
-def get_single_post(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        pass
-    return render(request, 'blog/detail.html', {'post':post})
-    
-
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
 def create_post(request):
     return HttpResponse('<h1>We have posts</h1>')
