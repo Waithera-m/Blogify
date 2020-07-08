e21c2930ae02b4df8ca0b4180b638f0e260c1f9c
Mary Waithera <njihiamary11@gmail.com>
2020-07-08 16:43:34 +0300

    Customize admin page to enhance functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    class customizes admin page
+    """
+    list_display = ['title', 'time_stamp', 'updated']
+    list_filter = ['time_stamp', 'updated']
+    search_fields = ['title', 'content']
+    list_editable = ['title']
+    list_display_links = ['time_stamp']
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """
+    class facilitates the creation of post objects
+    """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+   
+
